8e90891ea7669e11de9676b854205e8b6c1b843e
Bernard Ruffian <theruffian72@gmail.com>
2018-05-11 23:30:34 -0500

    bootstrap scripts for ACG and example HTML

---
+++ acloudguruscripts/custom metrics EC2 bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+yum install perl-Switch perl-DateTime perl-Sys-Syslog perl-LWP-Protocol-https -y
+mkdir /CloudWatch
+cd /CloudWatch
+curl http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip -O
+unzip CloudWatchMonitoringScripts-1.2.1.zip
+rm -rf CloudWatchMonitoringScripts-1.2.1.zip
+cd aws-scripts-mon
+./mon-put-instance-data.pl --mem-util --verify --verbose > CloudWatchtest.log
+./mon-put-instance-data.pl --mem-util --mem-used --mem-avail
+
